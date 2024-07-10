58363c733038a7e537ab1892327ed5ad0b40ec23
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2024-07-10 16:36:27 +0100

    k/alter: fix default remove
    The deprecated AlterConfigs API differs from the new
    IncrementalAlterConfigs API in that it is supposed to clear all configs
    not specified in the request. The comment in the function even states
    this requirement, but the behaviour was incorrect, as it was trying to
    `set` these fields (to the default value of the property's type T) when
    it should be `remove`'ing these properties to ensure that they are not
    specified at the topic-level and fall back to their cluster-level
    defaults.

--- src/v/kafka/server/handlers/alter_configs.cc
+++ src/v/kafka/server/handlers/alter_configs.cc
@@ -69,25 +69,27 @@ create_topic_properties_update(alter_configs_resource& resource) {
      * configuration in topic table, the only difference is the replication
      * factor, if not set in the request explicitly it will not be overriden.
      */
-    update.properties.compaction_strategy.op = op_t::set;
-    update.properties.compression.op = op_t::set;
-    update.properties.segment_size.op = op_t::set;
-    update.properties.timestamp_type.op = op_t::set;
-    update.properties.retention_bytes.op = op_t::set;
-    update.properties.retention_duration.op = op_t::set;
-    update.properties.shadow_indexing.op = op_t::set;
+    update.properties.compaction_strategy.op = op_t::remove;
+    update.properties.compression.op = op_t::remove;
+    update.properties.segment_size.op = op_t::remove;
+    update.properties.timestamp_type.op = op_t::remove;
+    update.properties.retention_bytes.op = op_t::remove;
+    update.properties.retention_duration.op = op_t::remove;
+    update.properties.shadow_indexing.op = op_t::remove;
 
     update.custom_properties.replication_factor.op = op_t::none;
     update.custom_properties.data_policy.op = op_t::none;
 
-    update.properties.record_key_schema_id_validation.op = op_t::set;
-    update.properties.record_key_schema_id_validation_compat.op = op_t::set;
-    update.properties.record_key_subject_name_strategy.op = op_t::set;
-    update.properties.record_key_subject_name_strategy_compat.op = op_t::set;
-    update.properties.record_value_schema_id_validation.op = op_t::set;
-    update.properties.record_value_schema_id_validation_compat.op = op_t::set;
-    update.properties.record_value_subject_name_strategy.op = op_t::set;
-    update.properties.record_value_subject_name_strategy_compat.op = op_t::set;
+    update.properties.record_key_schema_id_validation.op = op_t::remove;
+    update.properties.record_key_schema_id_validation_compat.op = op_t::remove;
+    update.properties.record_key_subject_name_strategy.op = op_t::remove;
+    update.properties.record_key_subject_name_strategy_compat.op = op_t::remove;
+    update.properties.record_value_schema_id_validation.op = op_t::remove;
+    update.properties.record_value_schema_id_validation_compat.op
+      = op_t::remove;
+    update.properties.record_value_subject_name_strategy.op = op_t::remove;
+    update.properties.record_value_subject_name_strategy_compat.op
+      = op_t::remove;
 
     schema_id_validation_config_parser schema_id_validation_config_parser{
       update.properties};
