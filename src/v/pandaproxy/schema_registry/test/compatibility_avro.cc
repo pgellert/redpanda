5af80eb83b6478567256996a9e88d36658330a17
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2025-01-02 09:08:05 +0000

    WIP

--- src/v/pandaproxy/schema_registry/test/compatibility_avro.cc
+++ src/v/pandaproxy/schema_registry/test/compatibility_avro.cc
@@ -18,6 +18,7 @@
 
 #include <absl/container/flat_hash_set.h>
 #include <avro/Compiler.hh>
+#include <avro/LogicalType.hh>
 #include <boost/test/tools/old/interface.hpp>
 
 #include <array>
@@ -284,11 +285,11 @@ SEASTAR_THREAD_TEST_CASE(test_avro_schema_definition_custom_attributes) {
     // custom attributes supported only at field level
     const auto avro_metadata_schema
       = pps::sanitize_avro_schema_definition(
-          {R"({"type":"record","name":"foo","ignored_attr":true,"fields":[{"name":"bar","type":"float","extra_attr":true}]})",
+          {R"({"type":"record","name":"Event","fields":[{"name":"eventTime","type":{"type":"long","logicalType":"timestamp-millis"}}]})",
            pps::schema_type::avro})
           .value();
     pps::canonical_schema_definition expected{
-      R"({"type":"record","name":"foo","fields":[{"name":"bar","type":"float","extra_attr":true}]})",
+      R"({"type":"record","name":"Event","fields":[{"name":"eventTime","type":{"type":"long","logicalType":"timestamp-millis"}}]})",
       pps::schema_type::avro};
     pps::sharded_store s;
     auto valid = pps::make_avro_schema_definition(
@@ -297,12 +298,9 @@ SEASTAR_THREAD_TEST_CASE(test_avro_schema_definition_custom_attributes) {
                     {avro_metadata_schema.shared_raw(),
                      pps::schema_type::avro}})
                    .get();
-    static_assert(
-      std::
-        is_same_v<std::decay_t<decltype(valid)>, pps::avro_schema_definition>,
-      "schema2 is an avro_schema_definition");
-    pps::canonical_schema_definition avro_conversion{valid};
-    BOOST_CHECK_EQUAL(expected, avro_conversion);
+    BOOST_CHECK(
+      avro::LogicalType::TIMESTAMP_MILLIS
+      == valid().root()->leafAt(0)->logicalType().type());
 }
 
 SEASTAR_THREAD_TEST_CASE(test_avro_alias_resolution_stopgap) {
