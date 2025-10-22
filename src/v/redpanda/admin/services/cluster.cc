f6604caef19a2846cc9e342de2926e01160b48b7
Gellért Peresztegi-Nagy <gellert.nagy@redpanda.com>
2025-10-22 12:00:15 +0100

    admin: implement cluster-wide collection of ListKafkaConnections

--- src/v/redpanda/admin/services/cluster.cc
+++ src/v/redpanda/admin/services/cluster.cc
@@ -13,6 +13,8 @@
 
 #include "features/feature_table.h"
 #include "proto/redpanda/core/admin/v2/cluster.proto.h"
+#include "proto/redpanda/core/admin/v2/kafka_connections.proto.h"
+#include "redpanda/admin/aip_ordering.h"
 #include "redpanda/admin/kafka_connections_service.h"
 #include "redpanda/admin/services/utils.h"
 #include "serde/protobuf/rpc.h"
@@ -39,6 +41,74 @@ cluster_service_impl::cluster_service_impl(
   , _kafka_connections_service(kafka_connections_service)
   , _feature_table(feature_table) {}
 
+ss::future<> cluster_service_impl::gather_all_brokers(
+  const serde::pb::rpc::context& ctx,
+  const response_consumer_t& add_to_response,
+  const proto::list_kafka_connections_request& req) {
+    auto make_broker_req = [&req](auto node_id) {
+        auto client_req = proto::admin::list_kafka_connections_request{};
+        client_req.set_node_id(node_id);
+        client_req.set_filter(ss::sstring{req.get_filter()});
+        client_req.set_order_by(ss::sstring{req.get_order_by()});
+        client_req.set_page_size(req.get_page_size());
+        return client_req;
+    };
+
+    // Iterate one by one for now to limit memory usage to be approximately in
+    // the order of 2 x page_size. We could optimize here to issue requests in
+    // parallel when page_size is small.
+    auto other_node_clients
+      = _proxy_client
+          .make_clients_for_other_nodes<proto::admin::cluster_service_client>();
+    for (auto& [node_id, client] : other_node_clients) {
+        auto client_resp = co_await client.list_kafka_connections(
+          ctx, make_broker_req(node_id));
+
+        co_await add_to_response(std::move(client_resp));
+    }
+
+    auto local_resp = co_await _kafka_connections_service.local()
+                        .list_kafka_connections_local(make_broker_req(-1));
+    co_await add_to_response(std::move(local_resp));
+}
+
+ss::future<proto::admin::list_kafka_connections_response>
+cluster_service_impl::list_kafka_connections_cluster_wide(
+  const serde::pb::rpc::context& ctx,
+  proto::admin::list_kafka_connections_request req) {
+    auto limit = _kafka_connections_service.local().get_effective_limit(
+      req.get_page_size());
+
+    auto collector = [&req, limit]() -> ss::shared_ptr<connection_collector> {
+        if (req.get_order_by().empty()) {
+            return ss::make_shared<unordered_collector>(limit);
+        } else {
+            auto ordering_conf
+              = make_ordering_config<proto::admin::kafka_connection>(
+                req.get_order_by());
+            auto comp = sort_order::parse(ordering_conf);
+
+            return ss::make_shared<ordered_collector<sort_order>>(limit, comp);
+        }
+    }();
+
+    auto total_count = size_t{0};
+
+    auto add_to_response = response_consumer_t{
+      [&collector, &total_count](
+        proto::admin::list_kafka_connections_response client_resp) {
+          total_count += client_resp.get_total_size();
+          return collector->add_all(std::move(client_resp.get_connections()));
+      }};
+
+    co_await gather_all_brokers(ctx, add_to_response, req);
+
+    auto resp = proto::admin::list_kafka_connections_response{};
+    resp.set_connections(co_await std::move(*collector).extract());
+    resp.set_total_size(total_count);
+    co_return resp;
+}
+
 ss::future<proto::admin::list_kafka_connections_response>
 cluster_service_impl::list_kafka_connections(
   serde::pb::rpc::context ctx,
@@ -51,6 +121,7 @@ cluster_service_impl::list_kafka_connections(
     auto target = req.has_node_id()
                     ? std::make_optional<model::node_id>(req.get_node_id())
                     : std::nullopt;
+    auto single_broker_request = target.has_value();
     if (target && *target != -1 && *target != _proxy_client.self_node_id()) {
         vlog(brlog.debug, "Redirecting to target node id {}", *target);
         co_return co_await _proxy_client
@@ -58,8 +129,11 @@ cluster_service_impl::list_kafka_connections(
           .list_kafka_connections(ctx, std::move(req));
     }
 
-    auto resp = co_await _kafka_connections_service.local()
-                  .list_kafka_connections_local(std::move(req));
+    auto resp = single_broker_request
+                  ? co_await _kafka_connections_service.local()
+                      .list_kafka_connections_local(std::move(req))
+                  : co_await list_kafka_connections_cluster_wide(
+                      ctx, std::move(req));
 
     vlog(
       brlog.trace,
